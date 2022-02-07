3f303ae5bac86eb467695f425bc04ec99ab9d50c
NaniCurti <81049910+IgnacioCurti@users.noreply.github.com>
2022-02-07 18:34:47 -0300

    Update main.js

--- challenges/clocks/js/main.js
+++ challenges/clocks/js/main.js
@@ -1,12 +1,12 @@
 function digitalClock(timeAR, timeNY, timeLA){
     const h1 = document.querySelector(".digital-clock h1")
     const h2 = document.querySelector(".digital-clock h2")
-    h1.textContent = timeAR.substring(0, 8);
-    h2.textContent = "Argentina"
     const h1NY = document.querySelector(".digital-clockNY h1")
     const h1CA = document.querySelector(".digital-clockCA h1")
     const h2NY = document.querySelector(".digital-clockNY h2")
     const h2CA = document.querySelector(".digital-clockCA h2")
+    h1.textContent = timeAR.substring(0, 8);
+    h2.textContent = "Argentina"
     h1NY.textContent = timeNY.substring(0, 8);
     h1CA.textContent = timeLA.substring(0, 8);
     h2NY.textContent = "New York"
